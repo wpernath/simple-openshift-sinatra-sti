843e686b522a71428eb5a09bb169e4343a326618
Wanja Pernath <w.pernath@gmx.de>
2017-07-19 14:16:38 +0200

    Hi Munich

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello Johannesburg, it is great to be here, Kruger Park was fantastic!!! Also Cape Town is great city and place to stay... RELEASE 1.4"
+  "Hallo München"
 end
