60f1346a83182527871d69c9dfd7f0901572130c
Wanja Pernath <w.pernath@gmx.de>
2017-11-21 15:17:53 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello Brussels, it's wet and cold and windy here. :-) "
+  "Hello Brussels, it's wet and cold and windy here. :-) -- RELASE 1.0 "
 end
