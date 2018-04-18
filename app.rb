0400d1f3980b3a51cdb31948d4b5c5e1957a5504
Wanja Pernath <w.pernath@gmx.de>
2018-04-18 15:05:23 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello Warsaw - why is it raining today??? -- RELASE 1.0 "
+  "Hello Warsaw - why is it raining today??? changed!-- RELASE 1.0 "
 end
