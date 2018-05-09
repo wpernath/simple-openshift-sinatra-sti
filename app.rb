78fab246d2903304d16cf95d357d6d62f44a2f5e
Wanja Pernath <w.pernath@gmx.de>
2018-05-09 09:48:41 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-  "Hello London... it's so sunny outside -- RELEASE 1.1 "
+  "Hello London... today it's the last day in London. In Munich it's raining. -- RELEASE 1.2 "
 end
