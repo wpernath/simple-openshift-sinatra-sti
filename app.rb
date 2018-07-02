99798d3018273457083aa1bdff6ec73617c34d72
Wanja Pernath <w.pernath@gmx.de>
2018-07-02 14:07:08 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   " Hallo Paderborn! Schön sonnig heute! -- Release 1.1"
+   " Hallo Paderborn! Schön sonnig heute uznd morgen auch-- Release 1.3"
 end
