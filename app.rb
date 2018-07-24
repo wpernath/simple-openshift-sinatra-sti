10d85a7390c560495adf3c9c87e8358f5c6156f9
Wanja Pernath <w.pernath@gmx.de>
2018-07-24 10:39:23 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   " Hallo Paderborn! Schön sonnig heute uznd morgen auch-- Release 1.3"
+   " Hello Paris, it's great to be here! -- RELEASE 1.0"
 end
