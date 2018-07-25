76e4b86d2f62df7df2ee48ea9b554dca8cb0c69f
dotslashcmd <37148458+dotslashcmd@users.noreply.github.com>
2018-07-25 16:01:28 +0200

    Update app.rb
    Some changes

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   " Hello Paris, it's great to be here! Some strange things -- RELEASE 1.1"
+   " Hello Paris, it's great to be here! New things -- RELEASE 1.1"
 end
