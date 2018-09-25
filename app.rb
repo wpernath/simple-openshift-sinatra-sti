956da1195c17188383f087fe84826bba9006e012
Wanja Pernath <w.pernath@gmx.de>
2018-09-25 14:31:58 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to the OpenShift Architecture Workshop - VERSION 2 "
+   "Welcome to the OpenShift Architecture Workshop - VERSION 3 "
 end
