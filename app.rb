2439f3179a971fa7056b28d88f92d555183800ae
wpernath <w.pernath@gmx.de>
2018-10-23 17:26:06 +0200

    standard version

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to the OpenShift Architecture Workshop in Moscow, any good steak restaurants nearby here? - VERSION 4 "
+   "Welcome to the OpenShift Architecture Workshop in XYZ - VERSION 1 "
 end
