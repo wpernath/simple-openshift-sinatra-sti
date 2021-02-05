c06e9f065953d5c207e2a620742fcd84dc0ac922
wpernath <w.pernath@gmx.de>
2021-02-05 10:50:33 +0100

    changed to pipes

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to OpenShift Architecture Workshop. -- This is changed now!"
+   "Welcome to OpenShift Architecture Workshop. -- Build via pipeline!"
 end
