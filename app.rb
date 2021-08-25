54dbd9b4994ca4a9b0c9db6acdfa1243ba3d81ff
Wanja Pernath <wpernath@redhat.com>
2021-08-25 10:19:15 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to OpenShift Architecture Workshop. Today it's sunny! yayyy!"
+   "Welcome to OpenShift Architecture Workshop. This is my second version!"
 end
