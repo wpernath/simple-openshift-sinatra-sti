370b6c52e0544ab35338b6245efae0b8ce6ea393
Wanja Pernath <w.pernath@gmx.de>
2022-04-05 09:56:06 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Hi there! Welcome to the Red Hat technical Bootcamp"
+   "Hi there! Welcome to the Red Hat technical Bootcamp - Version 2"
 end
