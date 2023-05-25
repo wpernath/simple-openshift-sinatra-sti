31eb0d4a0a8043604af071bb8c5594e611fc9b65
Wanja Pernath <w.pernath@gmx.de>
2023-05-25 11:12:18 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to OpenShift Developer Architecture Workshop! Hello guys out there. Updated!"
+   "Welcome to this nice application!"
 end
