b8d3f6c76df3140b01303ebfd5444767b1e5150c
Wanja Pernath <w.pernath@gmx.de>
2024-06-18 10:11:38 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-   "Welcome to OpenShift Arch Workshop! Thanks for joining!"
+   "Welcome to OpenShift Arch Workshop! Nice weather out there. At least here!"
 end
